df6b262a2287a36555ca881a5143d082cf5f19b9
Justin Fulton <jusfulton@gmail.com>
2020-12-07 16:55:44 -0800

    First Commit

---
+++ js/typewriter.js
@@ -0,0 +1,81 @@
+class TypeWriter {
+	constructor(txtElement, words, wait = 1000) {
+		this.txtElement = txtElement;
+		this.words = words;
+		this.txt = "";
+		this.wordIndex = 0;
+		this.wait = parseInt(wait, 10);
+		this.type();
+		this.isDeleting = false;
+	}
+
+	type() {
+		// Current index of word
+		const current = this.wordIndex % this.words.length;
+		// Get full text of current word
+		const fullTxt = this.words[current];
+
+		// Check if deleting
+		if (this.isDeleting) {
+			// Remove char
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			// Add char
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+
+		// Insert txt into element
+		this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+		// Initial Type Speed
+		let typeSpeed = 200;
+
+		if (this.isDeleting) {
+			typeSpeed /= 2;
+		}
+
+		// If word is complete
+		if (!this.isDeleting && this.txt === fullTxt) {
+			// Make pause at end
+			typeSpeed = this.wait;
+			// Set delete to true
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === "") {
+			this.isDeleting = false;
+			// Move to next word
+			this.wordIndex++;
+			// Pause before start typing
+			typeSpeed = 500;
+		}
+
+		setTimeout(() => this.type(), typeSpeed);
+	}
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+	const txtElement = document.querySelector(".txt-type");
+	const words = JSON.parse(txtElement.getAttribute("data-words"));
+	const wait = txtElement.getAttribute("data-wait");
+	// Init TypeWriter
+	new TypeWriter(txtElement, words, wait);
+}
+
+//Keeping this here for the future when I've got more time to solve a drop down or overlay menu.
+// let clicked = false;
+// const box = document.getElementById("jumbo-box");
+// function hideJumbo() {
+// 	const box = document.getElementById("jumbo-box");
+// 	if (clicked) {
+// 		setTimeout(function () {
+// 			box.style.display = "block";
+// 			clicked = false;
+// 		}, 400);
+// 	} else {
+// 		box.style.display = "none";
+// 		clicked = true;
+// 	}
+// }
